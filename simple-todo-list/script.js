22a7e3fa9f542a5d4fa29f996bf47cc9a5948554
adityazulvikar93 <adityazulvikar93@gmail.com>
2025-06-30 10:27:12 +0700

    Simple To-Do List v1.2

--- simple-todo-list/script.js
+++ simple-todo-list/script.js
@@ -10,19 +10,20 @@ addTaskBtn.addEventListener('click', function () {
     }
     // Add a task
     let li = document.createElement('li');
+    li.style.marginBottom = '10px';
     let taskSpan = document.createElement('span');
     taskSpan.textContent = taskText;
     // Button for 'done'
     let doneBtn = document.createElement('button');
-    doneBtn.textContent = 'Done';
+    doneBtn.textContent = '☑️ Done';
     doneBtn.style.marginLeft = '10px';
     // funtion for toggle 'done'
     function toggleDone() {
         taskSpan.classList.toggle('done');
         if (taskSpan.classList.contains('done')) {
-            doneBtn.textContent = 'Undone';
+            doneBtn.textContent = '↩️ Undone';
         } else {
-            doneBtn.textContent = 'Done';
+            doneBtn.textContent = '☑️ Done';
         }
     }
     // click task or button to toggle
@@ -30,15 +31,50 @@ addTaskBtn.addEventListener('click', function () {
     doneBtn.addEventListener('click', toggleDone);
     // Button for delete a task
     let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = '- Delete';
+    deleteBtn.textContent = '🗑️ Delete';
     deleteBtn.style.marginLeft = '10px';
     // Delete handler
     deleteBtn.addEventListener('click', function () {
         taskList.removeChild(li);
     });
+    // edit button
+    let editBtn = document.createElement('button');
+    editBtn.textContent = '✏️ Edit';
+    editBtn.style.marginLeft = '10px';
+    // function edit
+    let isEditing = false;
+    let editInput;
+    editBtn.addEventListener('click', function () {
+        if (!isEditing) {
+            editInput = document.createElement('input');
+            editInput.type = 'text';
+            editInput.value = taskSpan.textContent;
+            // replace span with edit
+            li.replaceChild(editInput, taskSpan);
+            // change edit icon to save icon
+            editBtn.textContent = "💾 Save";
+            isEditing = true;
+            // save using enter
+            editInput.addEventListener('keydown', function (e) {
+                if (e.key === 'Enter') {
+                    saveEdit();
+                }
+            });
+        } else {
+            saveEdit();
+        }
+        function saveEdit() {
+            taskSpan.textContent = editInput.value;
+            li.replaceChild(taskSpan, editInput);
+            editBtn.textContent = '✏️ Edit';
+            isEditing = false;
+        }
+    });
     li.appendChild(taskSpan);
     // Attach done button to side of a task
     li.appendChild(doneBtn);
+    // Attach edit button
+    li.appendChild(editBtn);
     // Attach delete button to side of a task
     li.appendChild(deleteBtn);
     // Add the list item to the task list
